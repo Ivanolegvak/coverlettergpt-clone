895e5d34e65430766b9a1a41c646d72f7057447b
Ivanolegvak <163690092+Ivanolegvak@users.noreply.github.com>
2025-05-18 10:18:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: "https://incandescent-youtiao-a647e8.netlify.app"
+  origin: "https://ai-generator-cover-letter.netlify.app"
 }));
 app.use(express.json());
 
